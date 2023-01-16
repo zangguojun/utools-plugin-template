d4e87b6cb994bb6a45ae40f456ac3d040628a8b1
tong <lbuchiyu@gmail.com>
2023-01-16 20:41:46 +0800

    init: init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App () {
+  return (
+    <>
+      <button className="btn btn-outline">Button</button>
+      <button className="btn btn-outline btn-primary">Button</button>
+      <button className="btn btn-outline btn-secondary">Button</button>
+      <button className="btn btn-outline btn-accent">Button</button>
+    </>
+  )
+}
+
+export default App
